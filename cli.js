e5161ef094cb14bafcfaf4aee818c37965449ac7
brownben <9870007+brownben@users.noreply.github.com>
2021-05-31 20:57:56 +0100

    refactor: move to es-modules

--- cli.js
+++ cli.js
@@ -1,9 +1,15 @@
-const cac = require('cac')
-const repl = require('repl')
-const fs = require('fs').promises
-const { execute, Interpreter, BangError } = require('./dist')
+import cac from 'cac'
+import repl from 'repl'
+import fs, { readFile } from 'fs/promises'
+import { execute, Interpreter, BangError } from './dist/bang.esm.js'
 
-const interpreter = new Interpreter()
+const readJSON = async (filePath) =>
+  JSON.parse(await readFile(new URL(filePath, import.meta.url)))
+
+const interpreter = new Interpreter({
+  fs,
+  printFunction: console.log,
+})
 
 const evaluate = (cmd, _context, _filename, callback) => {
   try {
@@ -20,20 +26,19 @@ const evaluate = (cmd, _context, _filename, callback) => {
   }
 }
 
-const cli = cac('bang')
-  .version(`v${require('./package.json').version}`)
-  .help()
+const packageJSON = await readJSON('./package.json')
+const cli = cac('bang').version(`v${packageJSON.version}`).help()
 
 cli.command('', 'Open a REPL').action(async () => {
-  console.log(`Bang (v${require('./package.json').version})`)
+  console.log(`Bang (v${packageJSON.version})`)
   console.log('For more information type ".help"\n')
   repl.start({ prompt: '> ', eval: evaluate })
 })
 
 cli.command('run <file>', 'Execute a Bang Program').action(async (file) => {
   try {
-    const fileContents = await fs.readFile(file, { encoding: 'utf8' })
-    execute(fileContents)
+    const fileContents = await readFile(file, { encoding: 'utf8' })
+    execute(fileContents, interpreter)
   } catch (error) {
     if (error instanceof BangError) error.output()
     else console.log(`Error: Problem Reading File "${file}"`)
