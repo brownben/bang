27a8bbda0670af8e130589bcf1a71c735b2d0c08
brownben <9870007+brownben@users.noreply.github.com>
2021-06-23 21:09:38 +0100

    types (prism): add types to prism package

--- packages/prism/src/index.ts
+++ packages/prism/src/index.ts
@@ -1,5 +1,7 @@
-export const addBang = (Prism) => {
-  Prism.languages.bang = Prism.languages.extend('javascript', {
+import Prism from 'prismjs'
+
+export const addBang = (prism: typeof Prism) => {
+  prism.languages.bang = prism.languages.extend('javascript', {
     keyword: [
       {
         pattern: /\b(?:if|else|while|let|const|return|import|as|from|try)\b/,
@@ -9,7 +11,7 @@ export const addBang = (Prism) => {
     operator: [
       {
         pattern:
-          /!|\*|\-|\+|\/|~|==|=|!=|!==|<=|>=|<|>|&&|\|\||\\*=|\/=|%=|\+=|\-=|=>|\.\.\.|\bor\b|\band\b|%|\?\?|\?\.|\./,
+          /!|\*|-|\+|\/|~|==|=|!=|!==|<=|>=|<|>|&&|\|\||\\*=|\/=|%=|\+=|-=|=>|\.\.\.|\bor\b|\band\b|%|\?\?|\?\.|\./,
       },
     ],
     boolean: [{ pattern: /\b(?:true|false|null)\b/ }],
